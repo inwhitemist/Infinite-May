d273ebfd2abd2a16e91f4eb7f0b355ca6a6bda3d
inwhitemist <gregory_05@bk.ru>
2023-01-10 00:00:52 +0300

    added stickness

--- js/app.js
+++ js/app.js
@@ -6,3 +6,12 @@ ScrollSmoother.create({
 	wrapper: '.wrapper',
 	content: '.content'
 })
+const height = $('.scrollable-main').height();
+ScrollTrigger.create({
+	trigger: ".main-article",
+	start: "top top",
+	end: height,
+	pinType: "transform",
+	onRefreshInit: self => self.scroll(0),
+	pin: true,
+});
